88da4596d025943ec139fafb5f01cc31ce1029bf
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-10-12 20:54:42 +0200

    Update layers

--- server/src/main/scala/infra/Layers.scala
+++ server/src/main/scala/infra/Layers.scala
@@ -3,8 +3,19 @@ package infra
 import zio._
 import zio.logging.backend.SLF4J
 import zio.logging.{ LogFormat, console }
+import infra.impl.JsonSchemaRegistry
+import service.impl.JsonSchemaValidator
+import service.impl.JsonDocumentCleaner
 
 object Layers {
   val logger = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
+  val registry: ZLayer[Any, Nothing, JsonSchemaRegistry.Registry] =
+    ZLayer(Ref.make(Map.empty))
+
+  val schemaRegistry = JsonSchemaRegistry.layer
+
+  val schemaValidator = JsonSchemaValidator.layer
+
+  val documentCleaner = JsonDocumentCleaner.layer
 }
