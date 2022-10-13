757748e3a4531de5b1cc2ec427bc81c82fa6d857
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-10-13 02:11:17 +0200

    Add document cleaner ~ second draft

--- server/src/main/scala/service/impl/JsonDocumentCleaner.scala
+++ server/src/main/scala/service/impl/JsonDocumentCleaner.scala
@@ -7,16 +7,46 @@ import zio.ZLayer
 import JsonDocumentCleaner.Z
 import model.domain
 import service.DocumentCleaner
+import scala.jdk.CollectionConverters._
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import scala.util.Try
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.fge.jsonschema.main.JsonSchemaFactory
+import com.github.fge.jsonschema.main.JsonValidator
+import model.json.JsonDocument
 
 case class JsonDocumentCleaner() extends DocumentCleaner[JSON, Z] {
 
-  def clean(document: Document[JSON]): Z[Document[JSON]] =
-    ZIO.succeed(document)
+  def clean(document: Document[JSON]): Z[Document[JSON]] = {
+    ZIO.fromTry(
+      Try {
+        val mapper = new ObjectMapper()
+        val keys   = document.value.fieldNames.asScala.toList
+        val values = document.value.elements.asScala.toList
 
+        val cleanDoc =
+          (keys zip values)
+            .foldLeft(mapper.createObjectNode()) { case (acc, (key, value)) =>
+              if (!value.isNull()) acc.set(key, value)
+              else acc
+            }
+        JsonDocument(cleanDoc)
+      }
+    )
+  }
 }
 
 object JsonDocumentCleaner {
   type Z[A] = ZIO[Any, Throwable, A]
+
+  val jsonValidator: ZLayer[Any, Throwable, JsonValidator] = ZLayer {
+    ZIO.fromTry(
+      Try {
+        JsonSchemaFactory.byDefault().getValidator()
+      }
+    )
+  }
 
   val layer: ZLayer[Any, Nothing, JsonDocumentCleaner] =
     ZLayer {
--- server/src/test/scala/service/impl/JsonDocumentCleanerSuite.scala
+++ server/src/test/scala/service/impl/JsonDocumentCleanerSuite.scala
@@ -9,13 +9,23 @@ object JsonDocumentCleanerSuite {
 
   def jsonSuite =
     suite("JSON Suite")(
+      testCleanDocument,
       testCleanDocumentIdempotent
     ).provide(JsonDocumentCleaner.layer)
 
   def testCleanDocumentIdempotent =
     test("clean document without nothing to clean") {
 
-      val json         = JacksonUtils.getReader().readTree("{}")
+      val json = JacksonUtils
+        .getReader()
+        .readTree("""
+          |{
+          |  "source": "/home/alice/image.iso",
+          |  "chunks": {
+          |    "size": 1024
+          |  }
+          |}
+        """.stripMargin)
       val jsonDocument = JsonDocument(json)
 
       ZIO.serviceWithZIO[JsonDocumentCleaner] { cleaner =>
@@ -24,4 +34,39 @@ object JsonDocumentCleanerSuite {
         } yield assertTrue(result == jsonDocument)
       }
     }
+
+  def testCleanDocument =
+    test("clean document from null values") {
+
+      val json = JacksonUtils
+        .getReader()
+        .readTree("""
+          |{
+          |  "source": null,
+          |  "chunks": {
+          |    "size": 1024
+          |  }
+          |}
+        """.stripMargin)
+      val jsonDocument = JsonDocument(json)
+
+      val expectedJson = JacksonUtils
+        .getReader()
+        .readTree("""
+          |{
+          |  "chunks": {
+          |    "size": 1024
+          |  }
+          |}
+        """.stripMargin)
+      val expectedJsonDocument = JsonDocument(expectedJson)
+
+      ZIO.serviceWithZIO[JsonDocumentCleaner] { cleaner =>
+        for {
+          result <- cleaner.clean(jsonDocument)
+        } yield assertTrue(result == expectedJsonDocument)
+      }
+    }
+
+  // @REVIEW clean empty objects ???
 }
