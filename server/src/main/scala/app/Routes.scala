717cfdefb4d3240316ac91cf9cab417be0a35efd
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-10-13 21:33:22 +0200

    Minor aesthetics

--- server/src/main/scala/app/Routes.scala
+++ server/src/main/scala/app/Routes.scala
@@ -6,11 +6,11 @@ import zhttp.http.{ Http, Method, Request, Response, !!, /, -> }
 object Routes {
 
   def apply(): Http[String, Nothing, Request, Response] =
-    Http.fromZIO(ZIO.service[String]).flatMap { _ =>
+    Http.fromZIO(ZIO.service[String]).flatMap { api =>
       Http.collect[Request] {
-        case Method.GET -> !! / "api" / "ready" =>
+        case Method.GET -> !! / `api` / "ready" =>
           Response.text(s"Service Ready")
-        case Method.GET -> !! / "api" / "health" =>
+        case Method.GET -> !! / `api` / "health" =>
           Response.text(s"Service Healthy")
       }
     }
